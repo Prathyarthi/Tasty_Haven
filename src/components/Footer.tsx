343f2d7474796e6756180241f44b66d36a083ebf
Prathyarthi <prathyarti@gmail.com>
2024-04-22 19:15:10 +0530

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+      <section className='mt-5 border-t-2 p-8 text-center'>
+          <footer>
+              &copy; 2024 All rights reserved
+          </footer>
+    </section>
+  )
+}
+
+export default Footer
