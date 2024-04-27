a305af776560f2881f220e5acd3b78bfc8440f8b
Prathyarthi <prathyarti@gmail.com>
2024-04-28 00:56:08 +0530

    Added user logic for signin using nextauth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,5 @@
+import connectToDb from "@/app/db"
+import UserModel from "@/app/models/userModel"
 import { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
@@ -5,13 +7,35 @@ export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
             name: 'Credentials',
+            id: 'credentials',
             credentials: {
                 email: { label: "Email", type: "email", placeholder: "user@gmail.com" },
                 password: { label: "Password", type: "password" }
             },
-            async authorize(credentials, req) {
+            async authorize(credentials, req): Promise<any> {
+                const email = credentials?.email
+                const password = credentials?.password
+                await connectToDb()
 
+                const user = await UserModel.findOne({
+                    email: email
+                })
+
+                if (!user) {
+                    return null
+                }
+
+                const isPasswordCorrect = await user.comparePassword(password || "")
+
+                if (isPasswordCorrect) {
+                    return user
+                }
+
+                return null
             }
         })
-    ]
+    ], secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: '/signin',
+    }
 }
